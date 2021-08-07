660b0d715010cdb6159b31b6f481ee7d2c023c76
sajjad <68008491+HackerGens@users.noreply.github.com>
2021-08-07 22:58:43 +0500

    1st commit

---
+++ app/src/main/java/crypto/currency/crypto_app/Tabs/MainActivity.java
@@ -0,0 +1,76 @@
+package crypto.currency.crypto_app.Tabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.viewpager.widget.ViewPager;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import crypto.currency.crypto_app.R;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    private int[] tabIcons = {
+            R.drawable.home,
+            R.drawable.coins,
+            R.drawable.history
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
+
+        viewPager=(ViewPager)findViewById(R.id.viewPager);
+        tabLayout.setUnboundedRipple(true);
+        tabLayout.hasUnboundedRipple();
+        tabLayout.getTabSelectedIndicator();
+
+        tabLayout.addTab(tabLayout.newTab().setIcon(tabIcons[0]));
+        tabLayout.addTab(tabLayout.newTab().setIcon(tabIcons[1]));
+        tabLayout.addTab(tabLayout.newTab().setIcon(tabIcons[2]));
+
+        tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#2485D3"));
+        tabLayout.setTabTextColors(Color.parseColor("#7E7C7C"), Color.parseColor("#f0639c"));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            tabLayout.getTabAt(0).getIcon().setTint(getResources().getColor(R.color.colorPrimary,getTheme()));
+        }
+
+        tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.transparent));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final TabsAdapter adapter = new TabsAdapter(this,getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    tab.getIcon().setTint(getResources().getColor(R.color.colorPrimary,getTheme()));
+                }
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    tab.getIcon().setTint(getResources().getColor(R.color.black,getTheme()));
+                }
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/crypto/currency/crypto_app/Tabs/TabsAdapter.java
@@ -0,0 +1,45 @@
+package crypto.currency.crypto_app.Tabs;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import crypto.currency.crypto_app.HomeFragment;
+import crypto.currency.crypto_app.SettingsFragment;
+
+public class TabsAdapter extends FragmentPagerAdapter {
+
+    private Context myContext;
+            int totalTabs;
+
+    public TabsAdapter(Context context, FragmentManager fm, int totalTabs) {
+            super(fm);
+            myContext = context;
+            this.totalTabs = totalTabs;
+            }
+
+        // this is for fragment tabs
+        @Override
+        public Fragment getItem(int position) {
+                switch (position) {
+                case 0:
+                HomeFragment homeFragment = new HomeFragment();
+                return homeFragment;
+                case 1:
+                SettingsFragment dappsFragment = new SettingsFragment();
+                return dappsFragment;
+                case 2:
+                SettingsFragment dexFragment = new SettingsFragment();
+                return dexFragment;
+                default:
+                 return null;
+                }
+        }
+            // this counts total number of tabs
+            @Override
+            public int getCount() {
+                    return totalTabs;
+            }
+}
